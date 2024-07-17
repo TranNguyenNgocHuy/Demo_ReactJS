2464c89de736113c898bdd2bf1db1181cfbdc356
TranHuy <tran.huy211014@gmail.com>
2024-07-17 16:00:00 +0700

    update key prod

--- split-bill-app/src/App.js
+++ split-bill-app/src/App.js
@@ -80,6 +80,7 @@ export default function App() {
 
       {selectedFriend && (
         <FormSplitBill
+          key={selectedFriend.id}
           selectedFriend={selectedFriend}
           handleSplitBill={handleSplitBill}
         />
